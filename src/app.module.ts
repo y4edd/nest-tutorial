bc95714d46ac0ef894511f1e34eae6dea031c94e
y4edd <yaeyuu0116@icloud.com>
2025-03-16 15:59:26 +0900

    feat:「nest generate module posts」によるpostsに関する機能をモジュール単位で管理できるように

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
 
 // アプリケーションのルートモジュールを定義します
 //  どこで宣言されているコントローラ・サービスを使うのかを定義する
 @Module({
-  imports: [],
+  imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
